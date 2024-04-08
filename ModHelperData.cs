762ced36a309d0b279ef14880959b73d89a3aed0
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2024-04-08 17:21:34 -0400

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@
 {
     public static class ModHelperData
     {
-        public const string WorksOnVersion = "39.2";
-        public const string Version = "1.0.6";
+        public const string WorksOnVersion = "42.0";
+        public const string Version = "1.0.7";
         public const string Name = "Primary Paragons";
         public const string Description =
             "This mod adds paragons for all of the primary towers.<br><br>This is originally Greenphx's mod, I have just fixed it to work in the newer BTD6 versions and added new features.";
